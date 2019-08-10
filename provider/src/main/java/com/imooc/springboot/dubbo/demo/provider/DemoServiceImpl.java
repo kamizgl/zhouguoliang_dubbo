105f7a8b1ccb5a9e831c8f4440e2eeb7e8898163
zgl <361251205@qq.com>
2019-08-10 23:28:54 +0800

    Revert "testcommit"
    This reverts commit c6a144e

--- provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoServiceImpl.java
+++ provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoServiceImpl.java
@@ -7,7 +7,6 @@ import com.imooc.springboot.dubbo.demo.DemoService;
 public class DemoServiceImpl implements DemoService {
 
     public String sayHello(String name) {
-        
         return "Hello, " + name + " (from Spring Boot)";
     }
 
