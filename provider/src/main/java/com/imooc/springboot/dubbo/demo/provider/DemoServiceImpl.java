c6a144e68f0860c87d8ee41042aba644c2300686
zgl <361251205@qq.com>
2019-08-10 23:16:27 +0800

    testcommit

--- provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoServiceImpl.java
+++ provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoServiceImpl.java
@@ -7,6 +7,7 @@ import com.imooc.springboot.dubbo.demo.DemoService;
 public class DemoServiceImpl implements DemoService {
 
     public String sayHello(String name) {
+        
         return "Hello, " + name + " (from Spring Boot)";
     }
 
